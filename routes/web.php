5107919066b9f16a94cfb7b1a06488813779b277
Washington <washington@localhost.localdomain>
2019-07-25 12:25:42 -0300

    Configuração iniciais do projeto

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,21 @@
 |
 */
 
+// Auth::routes();
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth/login');
 });
+
+// Authentication Routes...
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login');
+Route::get('logout', 'Auth\LoginController@logout')->name('logout');
+
+// Registration Routes...
+Route::get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
+Route::post('register', 'Auth\RegisterController@register');
+
+
+
+Route::get('/home', 'HomeController@index')->name('home');
